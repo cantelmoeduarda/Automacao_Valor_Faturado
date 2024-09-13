c43538065e14e5f6f444c6004314240495f1eebf
Eduarda <cantelmoeduarda@gmail.com>
2024-09-13 16:58:27 -0300

    Etapas finais da manipulação das planilhas - até o item Exercício

--- alteracao_planilha.py
+++ alteracao_planilha.py
@@ -1,8 +1,14 @@
 #alteração das planilhas sem o botão
 import pandas as pd
-df_apuracao = pd.read_excel('C:/Users/cante/OneDrive/Desktop/primeira/Apuração do faturamento 202408.xlsx', sheet_name = 'Apuração do Faturamento')
-df_gabarito = pd.read_excel('C:/Users/cante/OneDrive/Desktop/primeira/Gabarito Órgão-Sigla 202409.xlsx')
-df_cod_orgao = pd.read_excel('C:/Users/cante/OneDrive/Desktop/primeira/Gabarito Prateleira - SIAD - 202409.xlsx')
+#do pessoal:
+#df_apuracao = pd.read_excel('C:/Users/cante/OneDrive/Desktop/primeira/Apuração do faturamento 202408.xlsx', sheet_name = 'Apuração do Faturamento')
+#df_gabarito = pd.read_excel('C:/Users/cante/OneDrive/Desktop/primeira/Gabarito Órgão-Sigla 202409.xlsx')
+#df_cod_orgao = pd.read_excel('C:/Users/cante/OneDrive/Desktop/primeira/Gabarito Prateleira - SIAD - 202409.xlsx')
+
+#da seplag:
+df_apuracao = pd.read_excel('C:/Users/X15848318638/Desktop/primeira/Apuração do faturamento 202308.xlsx', sheet_name = 'Apuração do Faturamento')
+df_gabarito = pd.read_excel('C:/Users/X15848318638/Desktop/primeira/Gabarito Órgão-Sigla 202406.xlsx')
+df_cod_orgao = pd.read_excel('C:/Users/X15848318638/Desktop/primeira/Gabarito Prateleira - SIAD - 202409.xlsx')
 
 #posicao_col_valor_faturar = df_apuracao.columns.get_loc('Valor a Faturar pós IMR')
 #print(f'A posição é {posicao_col_valor_faturar}')
@@ -12,20 +18,40 @@ df_apuracao.insert(58,'Ajustes',0)
 # Etapa de preenchimento de uma nova coluna SIGLA com valores da tabela gabarito órgão sigla
 df_gabarito = df_gabarito.rename(columns = {'Cliente Nome':'Órgão/Entidade'})
 df_apuracao = df_apuracao.merge(df_gabarito[['Órgão/Entidade','Sigla']],on='Órgão/Entidade', how='left')
+
+
 # Etapa de preenchimento de uma nova coluna Item com valores da tabela gabarito prateleira
 df_cod_orgao = df_cod_orgao.rename(columns = {'Código do item AVMG':'ID Item'})
 df_cod_orgao = df_cod_orgao.rename(columns = {'Item Correspondente':'Item'})
 df_apuracao = df_apuracao.merge(df_cod_orgao[['ID Item','Item']],on='ID Item', how='left')
+
+
 # Etapa de preenchimento de uma nova coluna Categoria com valores da tabela gabarito prateleira
 df_apuracao = df_apuracao.merge(df_cod_orgao[['ID Item','Categoria']],on='ID Item', how='left')
+
+
 # Etapa Preencheendo  a coluna “Observação” com o texto “Sem observação”
-df_apuracao['Observação'] = 0
-#print(df_apuracao)
+df_apuracao['Observação'] = 'Sem observação'
+
+
+# Etapa Preencheendo  a coluna “Exercício” com o ano referente ao da coluna Data da Aprovação
+df_apuracao['Exercício'] = df_apuracao['Data da Aprovação'].dt.year.fillna(0).astype(int)
+
+
+# Etapa de exclusão das linhas do ano anterior
+## supondo que o usuário colocou o ano de 2023
+#df_apuracao = df_apuracao['Data do Fato gerador']==
+
+# Etapa de exclusão de valores "Não se aplica"
+df_apuracao['Data limite de entrega - pedido original'] = df['Data limite de entrega - pedido original'].replace('Não se aplica', "")
+
+
 df_apuracao.info()
-print(df_apuracao)
+print(df_apuracao['Data limite de entrega - pedido original'])
 
 
 
 
 #df_apuracao.info()
 #print(df_apuracao)
+
