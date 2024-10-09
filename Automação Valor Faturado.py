47a20720bfec1a749a677d99f22808b3099e929c
Eduarda <cantelmoeduarda@gmail.com>
2024-10-09 14:25:20 -0300

    troca do ícone, mensagem de alerta de coluna em branco, retirada de espaços extras em colunas chaves

--- Automação Valor Faturado.py
+++ Automação Valor Faturado.py
@@ -33,99 +33,130 @@ def main():
         raise Exception('Um dos arquivos não foram encontrados') #isso aqui vai parar o código
     
     
-    messagebox.showinfo('Sucesso',f'Arquivo Valor Faturado salvo na pasta "{os.path.basename(caminho_pasta)}" com sucesso')
-    df_apuracao.insert(58,'Ajustes',0)
-    # Etapa de preenchimento de uma nova coluna SIGLA com valores da tabela gabarito órgão sigla
-    df_orgao_sigla = df_orgao_sigla.rename(columns = {'Cliente Nome':'Órgão/Entidade'})
-    df_apuracao = df_apuracao.merge(df_orgao_sigla[['Órgão/Entidade','Sigla']],on='Órgão/Entidade', how='left') #esse merge deu tudo certo
+    try:
+        #retirando os possiveis espaços extras
+        df_orgao_sigla['Cliente Nome']=df_orgao_sigla['Cliente Nome'].apply(lambda x: x.strip() if isinstance(x, str) else x)
+        df_prateleira['Código do item AVMG']=df_prateleira['Código do item AVMG'].apply(lambda x: x.strip() if isinstance(x, str) else x)
+        df_apuracao['Órgão/Entidade']=df_apuracao['Órgão/Entidade'].apply(lambda x: x.strip() if isinstance(x, str) else x)                                        
+                                                    
+
+        # Etapa de preenchimento da coluna ajustes
+        df_apuracao['Ajustes']='0'
+        # Etapa de preenchimento de uma nova coluna SIGLA com valores da tabela gabarito órgão sigla
+        df_orgao_sigla = df_orgao_sigla.rename(columns = {'Cliente Nome':'Órgão/Entidade'})
+        df_apuracao = df_apuracao.merge(df_orgao_sigla[['Órgão/Entidade','Sigla']],on='Órgão/Entidade', how='left') #esse merge deu tudo certo
 
 
-    # Etapa de preenchimento de uma nova coluna Item e Categoria com valores da tabela gabarito prateleira
-    df_prateleira.drop(['Código Item Material - Numérico','Item Material','Item Correspondente','Situação'],axis='columns',inplace=True)
-    df_prateleira.drop_duplicates(subset=['Código do item AVMG'],inplace=True)
-    df_prateleira = df_prateleira.rename(columns = {'Código do item AVMG':'ID Item'})
-    df_prateleira = df_prateleira.rename(columns = {'Item Correspondente.1':'Item'})
-    df_apuracao = df_apuracao.merge(df_prateleira,on='ID Item', how='left')
+        # Etapa de preenchimento de uma nova coluna Item e Categoria com valores da tabela gabarito prateleira
+        df_prateleira.drop(['Código Item Material - Numérico','Item Material','Item Correspondente','Situação'],axis='columns',inplace=True)
+        df_prateleira.drop_duplicates(subset=['Código do item AVMG'],inplace=True)
+        df_prateleira = df_prateleira.rename(columns = {'Código do item AVMG':'ID Item'})
+        df_prateleira = df_prateleira.rename(columns = {'Item Correspondente.1':'Item'})
+        df_apuracao = df_apuracao.merge(df_prateleira,on='ID Item', how='left')
 
 
-    # Etapa Preencheendo  a coluna “Observação” com o texto “Sem observação”
-    df_apuracao['Observação'] = 'Sem observação'
+        # Etapa Preencheendo  a coluna “Observação” com o texto “Sem observação”
+        df_apuracao['Observação'] = 'Sem observação'
 
 
-    # Etapa Preencheendo  a coluna “Exercício” com o ano referente ao da coluna Data da Aprovação
-    df_apuracao['Exercício'] = df_apuracao['Data da Aprovação'].dt.year.fillna(0).astype(int)
+        # Etapa Preencheendo  a coluna “Exercício” com o ano referente ao da coluna Data da Aprovação
+        df_apuracao['Exercício'] = df_apuracao['Data da Aprovação'].dt.year.fillna(0).astype(int)
 
 
-    # Etapa de exclusão de valores "Não se aplica"
-    df_apuracao['Data limite de entrega - pedido original'] = df_apuracao['Data limite de entrega - pedido original'].replace('Não se aplica', "")
-    df_apuracao['Dias de atraso - pedido original'] = df_apuracao['Dias de atraso - pedido original'].replace('Não se aplica', "")
-    df_apuracao['Data limite de entrega - entrega corretiva'] = df_apuracao['Data limite de entrega - entrega corretiva'].replace('Não se aplica', "")
-    df_apuracao['Dias de atraso - entrega corretiva'] = df_apuracao['Dias de atraso - entrega corretiva'].replace('Não se aplica', "")
+        # Etapa de exclusão de valores "Não se aplica"
+        df_apuracao['Data limite de entrega - pedido original'] = df_apuracao['Data limite de entrega - pedido original'].replace('Não se aplica', "")
+        df_apuracao['Dias de atraso - pedido original'] = df_apuracao['Dias de atraso - pedido original'].replace('Não se aplica', "")
+        df_apuracao['Data limite de entrega - entrega corretiva'] = df_apuracao['Data limite de entrega - entrega corretiva'].replace('Não se aplica', "")
+        df_apuracao['Dias de atraso - entrega corretiva'] = df_apuracao['Dias de atraso - entrega corretiva'].replace('Não se aplica', "")
+        
+        # **Adicionar este trecho para verificar células em branco**
+        # Checando por células em branco em colunas específicas
+        columns_to_check = ['Sigla', 'Categoria', 'Item']  
+        missing_columns = []
 
-    
-    #Salvando o arquivo em excel com algumas colunas em formato de data e design em geral
-    caminho_arquivo_final = os.path.join(caminho_pasta,'Valor Faturado.xlsx')
-    with pd.ExcelWriter(caminho_arquivo_final, engine='openpyxl') as writer:
-        df_apuracao.to_excel(writer, index=False, sheet_name='Valor Faturado')
+        for column in columns_to_check:
+            if column in df_apuracao.columns:
+                if df_apuracao[column].isnull().any():
+                    missing_columns.append(column)
+            else:
+                print(f"Coluna {column} não encontrada no DataFrame df_apuracao.")
 
-    wb = load_workbook(caminho_arquivo_final)
-    ws = wb['Valor Faturado']
-    
-    #Etapa de tirar o time da data
-    colunas_de_data = ['Data do Fato Gerador', 'Data da Aprovação', 'Data do Ateste','Data limite de entrega - entrega corretiva', 'Data limite de entrega - pedido original']
-    for data in colunas_de_data:
-        df_apuracao[data] = pd.to_datetime(df_apuracao[data]).dt.strftime('%d/%m/%Y')
-
-    if 'date_style' not in wb.named_styles:
-        date_style = NamedStyle(name='date_style', number_format='DD/MM/YYYY')
-        wb.add_named_style(date_style)
-
-    colunas_de_data_excel = ['BJ', 'AV', 'AR', 'AQ']
-    for col in colunas_de_data:
-        if col in df_apuracao.columns:
-            df_apuracao[col] = pd.to_datetime(df_apuracao[col], format='%d/%m/%Y', errors='coerce')
-            
-    for col in colunas_de_data_excel:
-        for row in range(2, ws.max_row + 1): 
-            cell = ws[f'{col}{row}']
-            if cell.value:
-                cell.style = 'date_style'
-
-    # Adicionando formatação às células do título (linha 1)
-    dark_blue_fill = PatternFill(start_color='00003366', end_color='00003366', fill_type='solid')  # Azul escuro
-    yellow_fill = PatternFill(start_color='FFFF00', end_color='FFFF00', fill_type='solid')  # Amarelo
-
-    white_font_bold = Font(name='Arial', size=14, color='FFFFFF', bold=True)  # Texto branco em negrito, Arial, tamanho 14
-    black_font_bold = Font(name='Arial', size=14, color='000000', bold=True)  # Texto preto em negrito, Arial, tamanho 14
-
-    alignment = Alignment(horizontal='center', vertical='bottom')  # Centralizado e alinhado embaixo
-
-    yellow_columns = {'AB', 'AI', 'AN'}
-    yellow_columns.update([get_column_letter(col_num) for col_num in range(column_index_from_string('AQ'), column_index_from_string('BO') +1)])
-
-    # Definindo a altura da linha 1
-    ws.row_dimensions[1].height = 100
-
-    # Definindo a largura das colunas para 15
-    for col_num in range(1, ws.max_column + 1):
-        col_letter = get_column_letter(col_num)
-        ws.column_dimensions[col_letter].width = 15
-        cell = ws[f'{col_letter}1']
-        cell.alignment = alignment
-        if col_letter in yellow_columns:
-            cell.fill = yellow_fill
-            cell.font = black_font_bold
+        if missing_columns:
+            missing_message = f"\n\nObservação: as seguintes colunas possuem células em branco: {', '.join(missing_columns)}"
         else:
-            cell.fill = dark_blue_fill
-            cell.font = white_font_bold
+            missing_message = ""
 
-    filtro = f"A1:{get_column_letter(ws.max_column)}1"
-    ws.auto_filter.ref = filtro
+        
+        
+        #Salvando o arquivo em excel com algumas colunas em formato de data e design em geral
+        caminho_arquivo_final = os.path.join(caminho_pasta,'Valor Faturado.xlsx')
+        with pd.ExcelWriter(caminho_arquivo_final, engine='openpyxl') as writer:
+            df_apuracao.to_excel(writer, index=False, sheet_name='Valor Faturado')
 
-    wb.save(caminho_arquivo_final)
+        wb = load_workbook(caminho_arquivo_final)
+        ws = wb['Valor Faturado']
+        
+        #Etapa de tirar o time da data
+        colunas_de_data = ['Data do Fato Gerador', 'Data da Aprovação', 'Data do Ateste','Data limite de entrega - entrega corretiva', 'Data limite de entrega - pedido original']
+        for data in colunas_de_data:
+            df_apuracao[data] = pd.to_datetime(df_apuracao[data]).dt.strftime('%d/%m/%Y')
+
+        if 'date_style' not in wb.named_styles:
+            date_style = NamedStyle(name='date_style', number_format='DD/MM/YYYY')
+            wb.add_named_style(date_style)
+
+        colunas_de_data_excel = ['BI', 'AV', 'AR', 'AQ']
+        for col in colunas_de_data:
+            if col in df_apuracao.columns:
+                df_apuracao[col] = pd.to_datetime(df_apuracao[col], format='%d/%m/%Y', errors='coerce')
+                
+        for col in colunas_de_data_excel:
+            for row in range(2, ws.max_row + 1): 
+                cell = ws[f'{col}{row}']
+                if cell.value:
+                    cell.style = 'date_style'
+
+        # Adicionando formatação às células do título (linha 1)
+        dark_blue_fill = PatternFill(start_color='00003366', end_color='00003366', fill_type='solid')  # Azul escuro
+        yellow_fill = PatternFill(start_color='FFFF00', end_color='FFFF00', fill_type='solid')  # Amarelo
+
+        white_font_bold = Font(name='Arial', size=14, color='FFFFFF', bold=True)  # Texto branco em negrito, Arial, tamanho 14
+        black_font_bold = Font(name='Arial', size=14, color='000000', bold=True)  # Texto preto em negrito, Arial, tamanho 14
+
+        alignment = Alignment(horizontal='center', vertical='bottom')  # Centralizado e alinhado embaixo
+
+        yellow_columns = {'AB', 'AI', 'AN'}
+        yellow_columns.update([get_column_letter(col_num) for col_num in range(column_index_from_string('AQ'), column_index_from_string('BO') +1)])
+
+        # Definindo a altura da linha 1
+        ws.row_dimensions[1].height = 100
+
+        # Definindo a largura das colunas para 15
+        for col_num in range(1, ws.max_column + 1):
+            col_letter = get_column_letter(col_num)
+            ws.column_dimensions[col_letter].width = 15
+            cell = ws[f'{col_letter}1']
+            cell.alignment = alignment
+            if col_letter in yellow_columns:
+                cell.fill = yellow_fill
+                cell.font = black_font_bold
+            else:
+                cell.fill = dark_blue_fill
+                cell.font = white_font_bold
+
+        filtro = f"A1:{get_column_letter(ws.max_column)}1"
+        ws.auto_filter.ref = filtro
+
+        wb.save(caminho_arquivo_final)
+        messagebox.showinfo('Sucesso',f'Arquivo Valor Faturado salvo na pasta "{os.path.basename(caminho_pasta)}" com sucesso.{missing_message}')
+        
+        
+    except:
+        messagebox.showerror("Erro", "Erro ao gerar o arquivo. Verifique se os valores da planilha seguem o padrão recomendado.")
+        raise Exception('Erro ao gerar o arquivo')
+
     
-    
-    
+
     return
 
 
@@ -243,7 +274,7 @@ def resource_path(relative_path):
     return os.path.join(base_path,relative_path)
 
 # icone triagulo
-image_path = resource_path("triangulo.png")
+image_path = resource_path("avmg.png")
 imagem = Image.open(image_path)
 
 imagem = imagem.resize((32,32),Image.LANCZOS)
