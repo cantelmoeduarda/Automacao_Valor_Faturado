c1d46cb699c715d67ad331491aaa0a2a5cb6b517
Eduarda <cantelmoeduarda@gmail.com>
2024-10-09 15:25:50 -0300

    Colocando processbar

--- Automação Valor Faturado.py
+++ Automação Valor Faturado.py
@@ -1,5 +1,4 @@
-#importações
-
+# Importações
 from datetime import datetime as dt
 from dateutil.relativedelta import relativedelta
 import tkinter as tk
@@ -16,62 +15,56 @@ from openpyxl import load_workbook
 import sys
 import os
 import pandas as pd
-
-
-
+import threading  # Importar threading
+from tkinter import ttk  # Importar ttk para a barra de progresso
 
 def main():
-
-    caminho_pasta = var_caminho_pasta.get() 
-    df_apuracao = encontrar_caminho_apuracao()
-    df_orgao_sigla = encontrar_caminho_orgao_sigla()
-    df_prateleira = encontrar_caminho_gabarito()
-    
-
-    if df_apuracao is None or df_prateleira is None or df_orgao_sigla is None:
-        messagebox.showerror("Erro", "Um ou mais arquivos corretos não foram encontrados na pasta.")
-        raise Exception('Um dos arquivos não foram encontrados') #isso aqui vai parar o código
-    
-    
     try:
-        #retirando os possiveis espaços extras
-        df_orgao_sigla['Cliente Nome']=df_orgao_sigla['Cliente Nome'].apply(lambda x: x.strip() if isinstance(x, str) else x)
-        df_prateleira['Código do item AVMG']=df_prateleira['Código do item AVMG'].apply(lambda x: x.strip() if isinstance(x, str) else x)
-        df_apuracao['Órgão/Entidade']=df_apuracao['Órgão/Entidade'].apply(lambda x: x.strip() if isinstance(x, str) else x)                                        
-                                                    
+        caminho_pasta = var_caminho_pasta.get() 
+        df_apuracao = encontrar_caminho_apuracao()
+        df_orgao_sigla = encontrar_caminho_orgao_sigla()
+        df_prateleira = encontrar_caminho_gabarito()
+    
+        if df_apuracao is None or df_prateleira is None or df_orgao_sigla is None:
+            messagebox.showerror("Erro", "Um ou mais arquivos corretos não foram encontrados na pasta.")
+            raise Exception('Um dos arquivos não foram encontrados')  # Isso vai parar o código
+
+        # **Mostrar o indicador de carregamento**
+        show_loading_indicator()
+
+        # Retirando os possíveis espaços extras
+        df_orgao_sigla['Cliente Nome'] = df_orgao_sigla['Cliente Nome'].apply(lambda x: x.strip() if isinstance(x, str) else x)
+        df_prateleira['Código do item AVMG'] = df_prateleira['Código do item AVMG'].apply(lambda x: x.strip() if isinstance(x, str) else x)
+        df_apuracao['Órgão/Entidade'] = df_apuracao['Órgão/Entidade'].apply(lambda x: x.strip() if isinstance(x, str) else x)
 
         # Etapa de preenchimento da coluna ajustes
-        df_apuracao['Ajustes']='0'
-        # Etapa de preenchimento de uma nova coluna SIGLA com valores da tabela gabarito órgão sigla
-        df_orgao_sigla = df_orgao_sigla.rename(columns = {'Cliente Nome':'Órgão/Entidade'})
-        df_apuracao = df_apuracao.merge(df_orgao_sigla[['Órgão/Entidade','Sigla']],on='Órgão/Entidade', how='left') #esse merge deu tudo certo
+        df_apuracao['Ajustes'] = '0'
 
+        # Etapa de preenchimento de uma nova coluna SIGLA com valores da tabela gabarito órgão sigla
+        df_orgao_sigla = df_orgao_sigla.rename(columns={'Cliente Nome': 'Órgão/Entidade'})
+        df_apuracao = df_apuracao.merge(df_orgao_sigla[['Órgão/Entidade', 'Sigla']], on='Órgão/Entidade', how='left')
 
         # Etapa de preenchimento de uma nova coluna Item e Categoria com valores da tabela gabarito prateleira
-        df_prateleira.drop(['Código Item Material - Numérico','Item Material','Item Correspondente','Situação'],axis='columns',inplace=True)
-        df_prateleira.drop_duplicates(subset=['Código do item AVMG'],inplace=True)
-        df_prateleira = df_prateleira.rename(columns = {'Código do item AVMG':'ID Item'})
-        df_prateleira = df_prateleira.rename(columns = {'Item Correspondente.1':'Item'})
-        df_apuracao = df_apuracao.merge(df_prateleira,on='ID Item', how='left')
+        df_prateleira.drop(['Código Item Material - Numérico', 'Item Material', 'Item Correspondente', 'Situação'], axis='columns', inplace=True)
+        df_prateleira.drop_duplicates(subset=['Código do item AVMG'], inplace=True)
+        df_prateleira = df_prateleira.rename(columns={'Código do item AVMG': 'ID Item'})
+        df_prateleira = df_prateleira.rename(columns={'Item Correspondente.1': 'Item'})
+        df_apuracao = df_apuracao.merge(df_prateleira, on='ID Item', how='left')
 
-
-        # Etapa Preencheendo  a coluna “Observação” com o texto “Sem observação”
+        # Etapa preenchendo a coluna “Observação” com o texto “Sem observação”
         df_apuracao['Observação'] = 'Sem observação'
 
-
-        # Etapa Preencheendo  a coluna “Exercício” com o ano referente ao da coluna Data da Aprovação
+        # Etapa preenchendo a coluna “Exercício” com o ano referente ao da coluna Data da Aprovação
         df_apuracao['Exercício'] = df_apuracao['Data da Aprovação'].dt.year.fillna(0).astype(int)
-
 
         # Etapa de exclusão de valores "Não se aplica"
         df_apuracao['Data limite de entrega - pedido original'] = df_apuracao['Data limite de entrega - pedido original'].replace('Não se aplica', "")
         df_apuracao['Dias de atraso - pedido original'] = df_apuracao['Dias de atraso - pedido original'].replace('Não se aplica', "")
         df_apuracao['Data limite de entrega - entrega corretiva'] = df_apuracao['Data limite de entrega - entrega corretiva'].replace('Não se aplica', "")
         df_apuracao['Dias de atraso - entrega corretiva'] = df_apuracao['Dias de atraso - entrega corretiva'].replace('Não se aplica', "")
-        
-        # **Adicionar este trecho para verificar células em branco**
-        # Checando por células em branco em colunas específicas
-        columns_to_check = ['Sigla', 'Categoria', 'Item']  
+
+        # Verificar células em branco
+        columns_to_check = ['Sigla', 'Categoria', 'Item']
         missing_columns = []
 
         for column in columns_to_check:
@@ -86,18 +79,16 @@ def main():
         else:
             missing_message = ""
 
-        
-        
-        #Salvando o arquivo em excel com algumas colunas em formato de data e design em geral
-        caminho_arquivo_final = os.path.join(caminho_pasta,'Valor Faturado.xlsx')
+        # Salvando o arquivo em Excel com algumas colunas em formato de data e design em geral
+        caminho_arquivo_final = os.path.join(caminho_pasta, 'Valor Faturado.xlsx')
         with pd.ExcelWriter(caminho_arquivo_final, engine='openpyxl') as writer:
             df_apuracao.to_excel(writer, index=False, sheet_name='Valor Faturado')
 
         wb = load_workbook(caminho_arquivo_final)
         ws = wb['Valor Faturado']
-        
-        #Etapa de tirar o time da data
-        colunas_de_data = ['Data do Fato Gerador', 'Data da Aprovação', 'Data do Ateste','Data limite de entrega - entrega corretiva', 'Data limite de entrega - pedido original']
+
+        # Etapa de formatar datas
+        colunas_de_data = ['Data do Fato Gerador', 'Data da Aprovação', 'Data do Ateste', 'Data limite de entrega - entrega corretiva', 'Data limite de entrega - pedido original']
         for data in colunas_de_data:
             df_apuracao[data] = pd.to_datetime(df_apuracao[data]).dt.strftime('%d/%m/%Y')
 
@@ -109,29 +100,33 @@ def main():
         for col in colunas_de_data:
             if col in df_apuracao.columns:
                 df_apuracao[col] = pd.to_datetime(df_apuracao[col], format='%d/%m/%Y', errors='coerce')
-                
+
         for col in colunas_de_data_excel:
-            for row in range(2, ws.max_row + 1): 
+            for row in range(2, ws.max_row + 1):
                 cell = ws[f'{col}{row}']
                 if cell.value:
                     cell.style = 'date_style'
 
-        # Adicionando formatação às células do título (linha 1)
+        # Adicionando formatação aos nomes das colunas
         dark_blue_fill = PatternFill(start_color='00003366', end_color='00003366', fill_type='solid')  # Azul escuro
         yellow_fill = PatternFill(start_color='FFFF00', end_color='FFFF00', fill_type='solid')  # Amarelo
 
-        white_font_bold = Font(name='Arial', size=14, color='FFFFFF', bold=True)  # Texto branco em negrito, Arial, tamanho 14
-        black_font_bold = Font(name='Arial', size=14, color='000000', bold=True)  # Texto preto em negrito, Arial, tamanho 14
+        white_font_bold = Font(name='Arial', size=14, color='FFFFFF', bold=True)  # Texto branco em negrito
+        black_font_bold = Font(name='Arial', size=14, color='000000', bold=True)  # Texto preto em negrito
 
         alignment = Alignment(horizontal='center', vertical='bottom')  # Centralizado e alinhado embaixo
 
+        # Colunas que devem ser preenchidas em amarelo
         yellow_columns = {'AB', 'AI', 'AN'}
-        yellow_columns.update([get_column_letter(col_num) for col_num in range(column_index_from_string('AQ'), column_index_from_string('BO') +1)])
+        # Atualizar para incluir todas as colunas de 'AQ' até a última coluna da planilha
+        yellow_columns.update(
+            [get_column_letter(col_num) for col_num in range(column_index_from_string('AQ'), ws.max_column + 1)]
+        )
 
         # Definindo a altura da linha 1
         ws.row_dimensions[1].height = 100
 
-        # Definindo a largura das colunas para 15
+        # Definindo a largura das colunas para 15 e aplicando formatação
         for col_num in range(1, ws.max_column + 1):
             col_letter = get_column_letter(col_num)
             ws.column_dimensions[col_letter].width = 15
@@ -144,21 +139,47 @@ def main():
                 cell.fill = dark_blue_fill
                 cell.font = white_font_bold
 
+        # Aplicar filtro automático
         filtro = f"A1:{get_column_letter(ws.max_column)}1"
         ws.auto_filter.ref = filtro
 
         wb.save(caminho_arquivo_final)
-        messagebox.showinfo('Sucesso',f'Arquivo Valor Faturado salvo na pasta "{os.path.basename(caminho_pasta)}" com sucesso.{missing_message}')
-        
-        
-    except:
-        messagebox.showerror("Erro", "Erro ao gerar o arquivo. Verifique se os valores da planilha seguem o padrão recomendado.")
-        raise Exception('Erro ao gerar o arquivo')
 
-    
+        # **Ocultar o indicador de carregamento**
+        hide_loading_indicator()
+
+        # Exibir mensagem de sucesso
+        messagebox.showinfo('Sucesso', f'Arquivo Valor Faturado salvo na pasta "{os.path.basename(caminho_pasta)}" com sucesso.{missing_message}')
+        janela.destroy()  # Fecha a janela principal
+
+    except Exception as e:
+        # **Ocultar o indicador de carregamento em caso de erro**
+        hide_loading_indicator()
+
+        messagebox.showerror("Erro", f"Erro ao gerar o arquivo. Verifique se os valores da planilha seguem o padrão recomendado.\n\nDetalhes do erro: {str(e)}")
+        raise  # Relevanta a exceção para depuração
 
     return
 
+def show_loading_indicator():
+    # Exibir o indicador de carregamento
+    loading_label['text'] = "Processando..."
+    loading_label.grid(row=4, column=1, sticky='nsew')
+    progress_bar.grid(row=5, column=1, padx=10, pady=5, sticky='nsew')
+    progress_bar.start()
+    janela.update_idletasks()
+
+def hide_loading_indicator():
+    # Ocultar o indicador de carregamento
+    progress_bar.stop()
+    loading_label['text'] = ""
+    loading_label.grid_remove()
+    progress_bar.grid_remove()
+    janela.update_idletasks()
+
+def iniciar_processamento():
+    # Inicia a thread que executa a função main()
+    threading.Thread(target=main).start()
 
 def encontrar_caminho_apuracao():
     caminho_pasta = var_caminho_pasta.get()
@@ -170,21 +191,18 @@ def encontrar_caminho_apuracao():
                          and f.endswith('xlsx')]
 
     if not nome_apuracao:
-            print("Nenhum arquivo de 'Apuração do faturamento' encontrado.")
-            return None  # Retorna None se não encontrar arquivos
+        print("Nenhum arquivo de 'Apuração do faturamento' encontrado.")
+        return None  # Retorna None se não encontrar arquivos
 
     try:
         for arquivo in nome_apuracao:
             caminho_apuracao = os.path.join(caminho_pasta, arquivo)
-            df_apuracao = pd.read_excel(caminho_apuracao, sheet_name = 'Apuração do Faturamento')
+            df_apuracao = pd.read_excel(caminho_apuracao, sheet_name='Apuração do Faturamento')
             print(f"Arquivo {arquivo} lido com sucesso")
         return df_apuracao
     except PermissionError:
         messagebox.showerror("Erro", "Erro ao ler os arquivos. Verifique se o arquivo Apuração do Faturamento não esteja aberto")
         raise Exception('Erro ao ler arquivo Apuração do Faturamento')
-
-    
-
 
 def encontrar_caminho_gabarito():
     caminho_pasta = var_caminho_pasta.get()
@@ -206,9 +224,7 @@ def encontrar_caminho_gabarito():
         return df_prateleira
     except PermissionError:
         messagebox.showerror("Erro", "Erro ao ler os arquivos. Verifique se o arquivo Gabarito Prateleira não esteja aberto")
-        raise Exception('Erro ao ler arquivo gabarito Gabarito Prateleira')
-
-
+        raise Exception('Erro ao ler arquivo Gabarito Prateleira')
 
 def encontrar_caminho_orgao_sigla():
     caminho_pasta = var_caminho_pasta.get()
@@ -232,38 +248,34 @@ def encontrar_caminho_orgao_sigla():
         messagebox.showerror("Erro", "Erro ao ler os arquivos. Verifique se o arquivo Gabarito Órgão-Sigla esteja aberto")
         raise Exception('Erro ao ler Gabarito Órgão-Sigla')
 
-
-
 # A partir daqui o código é referente à janela 
 
-#Essa função 
 def selecionar_arquivo():
     caminho_pasta = askdirectory(title='Selecione a pasta com os arquivos')
     var_caminho_pasta.set(caminho_pasta)    
     if caminho_pasta:
-        label_pasta_selecionada['text'] = f"* Pasta selecionada: {os.path.basename(caminho_pasta)}" #ve se isso rola
-    
+        label_pasta_selecionada['text'] = f"* Pasta selecionada: {os.path.basename(caminho_pasta)}"
 
 janela = tk.Tk()
-janela.geometry('400x230')
-janela.title('Valor Faturado') #rever nome da janela
-janela.resizable(False,False) #pra não conseguirem mudar o tamanho da caixa
+janela.geometry('400x240')
+janela.title('Valor Faturado')
+janela.resizable(False, False)
 texto = tk.Label(janela, text='Selecione a pasta com os arquivos:')
 texto.grid(column=1, row=0, padx=10, pady=10, sticky='w')
-
-janela.grid_columnconfigure(0, weight=1)
-janela.grid_columnconfigure(1, weight=1)
+janela.grid_columnconfigure(1, weight=2)
 
 var_caminho_pasta = tk.StringVar()
 botao_selecionararquivo = tk.Button(janela, text="Selecionar", command=selecionar_arquivo)
 botao_selecionararquivo.grid(row=1, column=1, padx=10, pady=10, sticky='nsew')
-label_pasta_selecionada = tk.Label(janela, text='* Nenhuma pasta selecionada',fg='blue')
+label_pasta_selecionada = tk.Label(janela, text='* Nenhuma pasta selecionada', fg='blue')
 label_pasta_selecionada.grid(row=2, column=1, sticky='nsew')
 
-botao_processar = tk.Button(janela, text='Processar', command=main)
+botao_processar = tk.Button(janela, text='Processar', command=iniciar_processamento)
 botao_processar.grid(column=1, row=3, columnspan=1, padx=10, pady=10, ipady=5, sticky='nsew')
 
-
+# Criar o label e a barra de progresso, mas não posicioná-los ainda
+loading_label = tk.Label(janela, text="", fg='green')
+progress_bar = ttk.Progressbar(janela, orient='horizontal', mode='indeterminate')
 
 # Tratando as imagens que farão parte do botão:
 def resource_path(relative_path):
@@ -271,27 +283,25 @@ def resource_path(relative_path):
         base_path = sys._MEIPASS
     except Exception:
         base_path = os.path.abspath(".")
-    return os.path.join(base_path,relative_path)
+    return os.path.join(base_path, relative_path)
 
-# icone triagulo
+# Ícone da janela
 image_path = resource_path("avmg.png")
 imagem = Image.open(image_path)
-
-imagem = imagem.resize((32,32),Image.LANCZOS)
+imagem = imagem.resize((32, 32), Image.LANCZOS)
 icone = ImageTk.PhotoImage(imagem)
 janela.iconphoto(True, icone)
 
-# botao de ajuda
+# Botão de ajuda
 def ajuda():
     messagebox.showinfo("Informações importantes",
                         "Selecione a pasta que contenha os arquivos:\n\nApuração do Faturamento\nGabarito Prateleira - SIAD\nGabarito Órgão - Sigla")
-    
+
 icon_path = resource_path("botao_de_ajuda_transparente.png")
 help_icon = Image.open(icon_path)
 help_icon = help_icon.resize((22, 22), Image.LANCZOS)
 help_icon = ImageTk.PhotoImage(help_icon)
 help_button = tk.Button(janela, image=help_icon, command=ajuda, borderwidth=0, bg='#f0f0f0', activebackground='#f0f0f0')
-help_button.grid(row=4, column=4, padx=10, sticky='e')
-
+help_button.grid(row=6, column=5,padx=1, sticky='e')
 
 janela.mainloop()
